e50b9e98b2ce1058edcc2684aa7b039e5e546a3f
Ben Blackman <bblkmn5@gmail.com>
2017-09-01 03:44:50 +0000

    Done.

--- loops.js
+++ loops.js
@@ -23,9 +23,12 @@ function whileLoop(number) {
 
 
 function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
   do{
-    array.pop()
+    array = array.slice(1)
 
-  } while (array.length > 0)
+  } while (array.length > 0 && maybeTrue())
   return array
 }
